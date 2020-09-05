b2341152fa5b01042856c0f291175ec442c9d0f9
Lee <lee10202013@gmail.com>
2020-09-05 16:51:02 +0800

    bubble sort unit tests

--- algorithm/sorting/bubble/bubble.go
+++ algorithm/sorting/bubble/bubble.go
@@ -1,16 +1,11 @@
 package bubble
 
-import "fmt"
-
-func bubbleSort(slice []int) []int {
-	fmt.Println("Bubble Sort")
+func bubbleSort(slice []int) {
 	for i := 0; i < len(slice); i++ {
 		for j := 0; j < len(slice)-1; j++ {
-			if slice[j] < slice[j+1] {
+			if slice[j] > slice[j+1] {
 				slice[j], slice[j+1] = slice[j+1], slice[j]
 			}
 		}
 	}
-
-	return slice
 }
--- algorithm/sorting/bubble/bubble_test.go
+++ algorithm/sorting/bubble/bubble_test.go
@@ -1,15 +1,18 @@
 package bubble
 
 import (
+	"reflect"
 	"testing"
-
-	"github.com/c2fo/testify/suite"
 )
 
-type TestBubbleSuite struct {
-	suite.Suite
-}
+func TestBubbleSort(t *testing.T) {
+	testData := []int{4, 6, 3, 2, 5, 7, 9, 8, 1}
+	expectedData := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 
-func NewCoreTestSuite(t *testing.T) {
-	suite.Run(t, new(TestBubbleSuite))
+	bubbleSort(testData)
+
+	ok := reflect.DeepEqual(expectedData, testData)
+	if !ok {
+		t.Errorf("fail of bubble sort: [%v]", testData)
+	}
 }
